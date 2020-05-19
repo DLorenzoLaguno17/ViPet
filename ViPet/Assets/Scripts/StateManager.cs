b98188070c76bb08cc311ff056e49fcd7b4547f5
Èric Canela <knela96@gmail.com>
2020-05-19 19:37:23 +0200

    added unity

--- ViPet/Assets/Scripts/StateManager.cs
+++ ViPet/Assets/Scripts/StateManager.cs
@@ -48,13 +48,29 @@ public class StateManager : MonoBehaviour
     public List<EmotionCloud> clouds;
     public List<Emotions> emotions;
     IEnumerator routine = null;
-
     public EmotionStates current_emotion = 0;
+
+    private BarController AlimentationBar;
+    private BarController HappinessBar;
+    private BarController LoveBar;
+    public GameObject particle;
+
+    public float alimentation = 100;
+    public float happiness = 100;
+    public float love = 100;
+    private float lastTimeDecreased = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         ren = GetComponent<Renderer>();
+        AlimentationBar = GameObject.Find("AlimentationBar").GetComponent<BarController>();
+        HappinessBar = GameObject.Find("HappinessBar").GetComponent<BarController>();
+        LoveBar = GameObject.Find("LoveBar").GetComponent<BarController>();
+
+        AlimentationBar.SetMaxValue(alimentation);
+        HappinessBar.SetMaxValue(happiness);
+        LoveBar.SetMaxValue(love);
     }
 
     // Update is called once per frame
@@ -78,19 +94,65 @@ public class StateManager : MonoBehaviour
             {
                 if (current_emotion == emotions[i].emotion)
                 {
-                    if (routine != null)
-                        StopCoroutine(routine);
-                    routine = setEmotion(i);
-                    StartCoroutine(routine);
+                    newEmotion(current_emotion);
                     break;
                 }
             }
         }
 
+        if (Time.time > lastTimeDecreased + 0.1)
+        {
+            lastTimeDecreased = Time.time;
+
+            alimentation -= 0.1f;
+            happiness -= 0.1f;
+            love -= 0.1f;
+
+            AlimentationBar.SetSize(alimentation);
+            HappinessBar.SetSize(happiness);
+            LoveBar.SetSize(love);
+        }
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Create a ray from the current touch coordinates
+                Ray ray = Camera.allCameras[0].ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject.name == "Buizel")
+                    {
+                        Instantiate(particle, transform.position, transform.rotation);
+                        love += 20.0f;
+                        if (love > 100)
+                            love = 100;
+
+                        newEmotion(EmotionStates.Love);
+                    }
+                    else if (hit.collider.gameObject.name == "Ball")
+                    {
+                        newEmotion(EmotionStates.Angry);
+                    }
+                }
+
+            }
+        }
+
     }
 
-    private IEnumerator setEmotion(int i)
+    private void newEmotion(EmotionStates emotion)
     {
+        if (routine != null)
+            StopCoroutine(routine);
+        routine = playEmotion(emotion);
+        StartCoroutine(routine);
+    }
+
+    private IEnumerator playEmotion(EmotionStates emotion)
+    {
+        int i = (int)emotion;
         GameObject cloud = gameObject.transform.Find("Cloud").gameObject;
         if(emotions[i].Cloud != -1)
             cloud.SetActive(true);
@@ -99,7 +161,7 @@ public class StateManager : MonoBehaviour
 
         AssigEmotion(i);
 
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(1.5f);
 
         i = (int)EmotionStates.Normal;
         AssigEmotion(i);
