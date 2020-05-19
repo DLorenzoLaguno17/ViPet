37e9cbd3b729a36541b6f8d669bb3c8a0a73fd31
Dani Lorenzo <dlorenzolaguno17@gmail.com>
2020-05-19 19:08:11 +0200

    Enabled multitargeting, added UI and implemented ball movement

---
+++ ViPet/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject food;
+
+    public void SpawnBall()
+    {
+        Instantiate(ball, transform.position, transform.rotation);
+        GameObject.Find("GameManager").GetComponent<GameManager>().love += 20.0f;
+    }
+
+    public void SpawnFood()
+    {
+        Instantiate(food, transform.position, transform.rotation);
+        GameObject.Find("GameManager").GetComponent<GameManager>().love -= 20.0f;
+    }
+}
---
+++ ViPet/Assets/Scripts/TargetManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public GameObject go;
+
+    public void CreateObject()
+    {
+        Instantiate(go, transform.position, transform.rotation);
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(go);
+    }
+}
